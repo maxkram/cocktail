01c0125b87a1cdbb99caa03ebd4c347d6fee6c8b
maxkram <max.kramer@live.com>
2022-08-19 10:02:36 -0400

    feat: add routes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    return (
+        <section className='section about-section'>
+            <h1 className='section-title'>about page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+                consectetur assumenda iste voluptate eveniet vitae quam odio
+                voluptatibus, cum, officiis nam, sint deleniti maxime. Suscipit
+                corrupti, reiciendis fugit aliquid hic doloremque. Recusandae
+                vel eos fuga at dolorem sunt doloribus distinctio?
+            </p>
+        </section>
+    );
+};
 
-export default About
+export default About;
